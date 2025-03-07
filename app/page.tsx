0ba1904ed1012fb32cc86d4c5425f799d8303e8d
tomlenek <lequangminh2204@gmail.com>
2025-03-07 21:27:35 +0700

    feat(advanced-3d-customize-ui): full setup for 3d ui of user (still fixing bugs)

--- app/page.tsx
+++ app/page.tsx
@@ -1,4 +1,5 @@
-import Image from "next/image";
+import CakeHomeDashboard from "@/components/3d-custom/cake-home-dashboard";
+import Model3DCustom from "./Model3DCustom/page";
 
 export default function Home() {
   return <div>main page</div>;
