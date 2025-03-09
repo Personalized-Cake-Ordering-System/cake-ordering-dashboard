c7b467eb80774cf4202a1417d53b380b61396c08
tomlenek <lequangminh2204@gmail.com>
2025-03-09 07:48:36 +0700

    update

--- features/barkeries/actions/barkeries-action.ts
+++ features/barkeries/actions/barkeries-action.ts
@@ -1,24 +1,24 @@
-// "use server";
-// import { unstable_noStore as noStore, revalidatePath } from "next/cache";
+"use server";
+import { unstable_noStore as noStore, revalidatePath } from "next/cache";
 
-// import { ApiListResponse, fetchListData } from "@/lib/api/api-handler/generic";
-// import { SearchParams } from "@/types/table";
-// import { IBarkery } from "../types/barkeries-type";
+import { ApiListResponse, fetchListData } from "@/lib/api/api-handler/generic";
+import { SearchParams } from "@/types/table";
+import { IBarkery } from "../types/barkeries-type";
 
-// export const getBakeries = async (
-//   searchParams: SearchParams
-// ): Promise<ApiListResponse<IBarkery>> => {
-//   noStore();
+export const getBakeries = async (
+  searchParams: SearchParams
+): Promise<ApiListResponse<IBarkery>> => {
+  noStore();
 
-//   const result = await fetchListData<IBarkery>("/bakeries", searchParams);
+  const result = await fetchListData<IBarkery>("/bakeries", searchParams);
 
-//   if (!result.success) {
-//     console.error("Failed to fetch list IBarkery:", result.error);
-//     return { data: [], pageCount: 0, error: result.error };
-//   }
+  if (!result.success) {
+    console.error("Failed to fetch list IBarkery:", result.error);
+    return { data: [], pageCount: 0, error: result.error };
+  }
 
-//   return result.data;
-// };
+  return result.data;
+};
 
 "use server";
 import { cache } from "react";
